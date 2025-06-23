002c679a2ffd50a1a5a7b0fbab45c0379eba212d
HenryTea <104259617+HenryTea@users.noreply.github.com>
2025-06-24 06:09:07 +0700

    still trying to fix db.js

--- diary-nextjs/db/db.js
+++ diary-nextjs/db/db.js
@@ -1,5 +1,10 @@
 import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool(process.env.DATABASE_URL);
+const pool = mysql.createPool({
+  ...mysql.createConnection({uri: process.env.DATABASE_URL}),
+  ssl: {
+    rejectUnauthorized: true
+  }
+});
 
 export default pool;
