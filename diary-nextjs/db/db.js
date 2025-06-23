757d922390017439cd8840a27435f3bdd8175425
HenryTea <104259617+HenryTea@users.noreply.github.com>
2025-06-24 06:16:48 +0700

    one more time!!

--- diary-nextjs/db/db.js
+++ diary-nextjs/db/db.js
@@ -1,11 +1,7 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'maglev.proxy.rlwy.net',
-  user: 'root',
-  password: 'YOUR_PASSWORD',
-  database: 'railway',
-  port: 55569,
+  uri: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: true
   }
