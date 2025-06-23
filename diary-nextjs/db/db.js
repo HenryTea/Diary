9320ee524fd3f6960dfe21dd3825c8bd48b0694a
HenryTea <104259617+HenryTea@users.noreply.github.com>
2025-06-24 05:17:45 +0700

    Update db.js

--- diary-nextjs/db/db.js
+++ diary-nextjs/db/db.js
@@ -1,14 +1,5 @@
 import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool({
-  host: 'localhost', // MySQL server address (do not include port here)
-  port: 3306,        // MySQL port (default is 3306)
-  user: 'root',      // your MySQL username
-  password: 'H.tl2537983098', // your MySQL password
-  database: 'diary', // your database name
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 export default pool;
